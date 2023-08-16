12be96e82c2baccfa1e3dc60598f44fcfeb8845d
Elena Pilyugina <elena.pilyugina.job@gmail.com>
2023-08-16 14:38:12 +0300

    Initial commit

---
+++ src/main/java/com/github/pielena/postal/tracking/PostalTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.github.pielena.postal.tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostalTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostalTrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/pielena/postal/tracking/PostalTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.pielena.postal.tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostalTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
